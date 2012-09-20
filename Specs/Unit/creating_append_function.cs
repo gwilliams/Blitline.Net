d25bc996a651cea4e2dbf5416168bba3bd0a19d0
Gareth Williams <gw@gdoubleu.co.uk>
2012-09-20 08:45:58 +0100

    started to add unit tests to check param construction

---
+++ Specs/Unit/creating_append_function.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Blitline.Net.Functions;
+using Machine.Specifications;
+
+namespace Specs.Unit.Append
+{
+    [Subject(typeof(AppendFunction))]
+    public class when_creating_append_function
+    {
+        static AppendFunction annotate;
+
+        Because of = () => annotate = new AppendFunction(true);
+
+        It it_should_set_values_on_params = () =>
+        {
+            Type t = annotate.Params.GetType();
+            ((bool)t.GetProperty("vertical").GetValue(annotate.Params, null)).ShouldEqual(true);
+        };
+    }
+}
---
+++ Specs/Unit/creating_blur_function.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Blitline.Net.Functions;
+using Machine.Specifications;
+
+namespace Specs.Unit.Blur
+{
+     [Subject(typeof(BlurFunction))]
+    public class when_creating_blur_function
+    {
+        static BlurFunction annotate;
+
+        Because of = () => annotate = new BlurFunction(10, 5);
+
+        It it_should_set_values_on_params = () =>
+        {
+            Type t = annotate.Params.GetType();
+            ((decimal)t.GetProperty("sigma").GetValue(annotate.Params, null)).ShouldEqual(10);
+            ((decimal)t.GetProperty("radius").GetValue(annotate.Params, null)).ShouldEqual(5);
+        };
+    }
+}
