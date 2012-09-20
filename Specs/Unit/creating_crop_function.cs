d6246de41e5a96bef946bb6ce3f6d928fa13df2e
Gareth Williams <gw@gdoubleu.co.uk>
2012-09-20 09:14:52 +0100

    yet more tests

---
+++ Specs/Unit/creating_crop_function.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Blitline.Net.Functions;
+using Machine.Specifications;
+
+namespace Specs.Unit.crop
+{
+    [Subject(typeof(CropFunction))]
+    public class when_creating_blur_function
+    {
+        static CropFunction function;
+
+        Because of = () => function = new CropFunction(1, 2, 3, 4);
+
+        It it_should_set_values_on_params = () =>
+        {
+            Type t = function.Params.GetType();
+            ((int)t.GetProperty("x").GetValue(function.Params, null)).ShouldEqual(1);
+            ((int)t.GetProperty("y").GetValue(function.Params, null)).ShouldEqual(2);
+            ((int)t.GetProperty("width").GetValue(function.Params, null)).ShouldEqual(3);
+            ((int)t.GetProperty("height").GetValue(function.Params, null)).ShouldEqual(4);
+        };
+    }
+}
---
+++ Specs/Unit/creating_deskew_function.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Blitline.Net.Functions;
+using Machine.Specifications;
+
+namespace Specs.Unit.Deskew
+{
+    [Subject(typeof(DeskewFunction))]
+    public class when_only_supplying_default_values
+    {
+        static DeskewFunction function;
+
+        Because of = () => function = new DeskewFunction();
+
+        It it_should_set_default_values_on_params = () =>
+        {
+            Type t = function.Params.GetType();
+            ((decimal)t.GetProperty("threshold").GetValue(function.Params, null)).ShouldEqual(0.4m);
+
+        };
+    }
+
+    [Subject(typeof(DeskewFunction))]
+    public class when_only_supplying_custom_values
+    {
+        static DeskewFunction function;
+
+        Because of = () => function = new DeskewFunction(10);
+
+        It it_should_set_default_values_on_params = () =>
+        {
+            Type t = function.Params.GetType();
+            ((decimal)t.GetProperty("threshold").GetValue(function.Params, null)).ShouldEqual(10);
+        };
+    }
+}
