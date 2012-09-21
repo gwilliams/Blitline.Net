0e5f3c0b8cc56cdb4dbabdbbbe65397c30e88c27
Gareth Williams <gw@gdoubleu.co.uk>
2012-09-21 10:56:41 +0100

    tests for median filter and module functions

---
+++ Specs/Unit/creating_median_filter_function.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Blitline.Net.Functions;
+using Machine.Specifications;
+
+namespace Specs.Unit.MedianFilter
+{
+    [Subject(typeof(MedianFilterFunction))]
+    public class when_only_supplying_default_values
+    {
+        static MedianFilterFunction function;
+
+        Because of = () => function = new MedianFilterFunction();
+
+        It it_should_set_default_values_on_params = () =>
+        {
+            Type t = function.Params.GetType();
+            ((decimal)t.GetProperty("radius").GetValue(function.Params, null)).ShouldEqual(1.0m);
+
+        };
+    }
+
+    [Subject(typeof(MedianFilterFunction))]
+    public class when_only_supplying_custom_values
+    {
+        static MedianFilterFunction function;
+
+        Because of = () => function = new MedianFilterFunction(5.1m);
+
+        It it_should_set_default_values_on_params = () =>
+        {
+            Type t = function.Params.GetType();
+            ((decimal)t.GetProperty("radius").GetValue(function.Params, null)).ShouldEqual(5.1m);
+
+        };
+    }
+}
---
+++ Specs/Unit/creating_modulate_function.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Blitline.Net.Functions;
+using Machine.Specifications;
+
+namespace Specs.Unit.Modulate
+{
+    [Subject(typeof(ModulateFunction))]
+    public class when_only_supplying_default_values
+    {
+        static ModulateFunction function;
+
+        Because of = () => function = new ModulateFunction();
+
+        It it_should_set_default_values_on_params = () =>
+        {
+            Type t = function.Params.GetType();
+            ((decimal)t.GetProperty("brightness").GetValue(function.Params, null)).ShouldEqual(1.0m);
+            ((decimal)t.GetProperty("saturation").GetValue(function.Params, null)).ShouldEqual(1.0m);
+            ((decimal)t.GetProperty("hue").GetValue(function.Params, null)).ShouldEqual(1.0m);
+
+        };
+    }
+
+    [Subject(typeof(ModulateFunction))]
+    public class when_only_supplying_custom_values
+    {
+        static ModulateFunction function;
+
+        Because of = () => function = new ModulateFunction(2m, 3m, 4m);
+
+        It it_should_set_default_values_on_params = () =>
+        {
+            Type t = function.Params.GetType();
+            ((decimal)t.GetProperty("brightness").GetValue(function.Params, null)).ShouldEqual(2.0m);
+            ((decimal)t.GetProperty("saturation").GetValue(function.Params, null)).ShouldEqual(3.0m);
+            ((decimal)t.GetProperty("hue").GetValue(function.Params, null)).ShouldEqual(4.0m);
+
+        };
+    }
+}
