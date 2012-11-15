ec2afbde08e625b9b4569e836c5487b89e221591
Gareth Williams <gareth.williams2@easyjet.com>
2012-11-15 13:00:57 +0000

    Adding in xunit and subspec

--- Specs/Integration/saving_to_s3.cs
+++ Specs/Integration/saving_to_s3.cs
@@ -1,51 +1,49 @@
 ﻿using System.Linq;
-using System.Net.Http;
 using Blitline.Net;
 using Blitline.Net.Functions;
 using Blitline.Net.Request;
 using Blitline.Net.Response;
 using Machine.Specifications;
-
+using SubSpec;
 namespace Specs.Integration
 {
-    [Subject("Saving to S3")]
-    public class saving_to_s3
+    public class SavingToS3
     {
-        static BlitlineApi _blitline;
-        static HttpClient _client;
-        static BlitlineResponse _response;
-        static BlitlineRequest _request;
-        const string BucketName = "elevate-test-photos";
-
-        Establish context = () =>
+        [Specification]
+        public void CanSaveToS3Bucket()
         {
-            _blitline = new BlitlineApi();
-            _request = new BlitlineRequest("bqbTZJ-fe3sBFfJ2G0mKWw", "https://s3-eu-west-1.amazonaws.com/elevate-test-photos/gw%40elevatedirect.com-new.png");
+            var blitlineApi = default(BlitlineApi);
+            var request = default(BlitlineRequest);
+            var response = default(BlitlineResponse);
+            var bucketName = default(string);
 
-            var cropFunction = new CropFunction(51, 126, 457 - 126, 382 - 51)
-            {
-                Save = new Save
+            "Given I have a blitline request with an s3 destination".Context(() =>
                 {
-                    ImageIdentifier = "image_identifier",
-                    S3Destination = new S3Destination
-                                         {
-                                             Bucket = "elevate-test-photos",
-                                             Key = "blitline.png"
-                                         }
-                }
-            };
+                    bucketName = "elevate-test-photos";
+                    blitlineApi = new BlitlineApi();
+                    request = new BlitlineRequest("bqbTZJ-fe3sBFfJ2G0mKWw", "https://s3-eu-west-1.amazonaws.com/elevate-test-photos/gw%40elevatedirect.com-new.png");
 
-            _request.AddFunction(cropFunction);
+                    var cropFunction = new CropFunction(51, 126, 457 - 126, 382 - 51)
+                    {
+                        Save = new Save
+                        {
+                            ImageIdentifier = "image_identifier",
+                            S3Destination = new S3Destination
+                            {
+                                Bucket = "elevate-test-photos",
+                                Key = "blitline.png"
+                            }
+                        }
+                    };
+                    
+                    request.AddFunction(cropFunction);
+                });
 
-        };
+            "When I process the request".Do(() => response = blitlineApi.ProcessImages(request));
 
-        Because of = () => _response = _blitline.ProcessImages(_request);
+            "Then the s3 url should not be empty".Observation(() => response.Results.Images.First().S3Url.ShouldNotBeEmpty());
 
-        It response_should_contain_correct_s3_url = () =>
-                                                        {
-                                                            _response.Results.Images.First().S3Url.ShouldNotBeEmpty();
-                                                            _response.Results.Images.First().S3Url.ShouldContain(BucketName);
-                                                        };
-
+            "And the s3 url should contain the bucket name".Observation(() => response.Results.Images.First().S3Url.ShouldContain(bucketName));
+        }
     }
 }
