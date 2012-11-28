58961dbca36c4121d1aeb4fe9933c1ee56f132fb
Gareth Williams <gareth.williams2@easyjet.com>
2012-11-28 12:37:15 +0000

    Added integration test to check the screen_shot_url processing

---
+++ Specs/Integration/ProcessScreenshot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Blitline.Net.Builders;
+using Blitline.Net.Request;
+using Blitline.Net.Response;
+using SubSpec;
+using Xunit;
+
+namespace Specs.Integration
+{
+    public class ProcessScreenshot
+    {
+        [Specification]
+        public void CanSaveToS3Bucket()
+        {
+            var request = default(BlitlineRequest);
+            var response = default(BlitlineResponse);
+
+            "Given I have a screenshot request".Context(() =>
+                                                            {
+                                                                const string bucketName = "gdoubleu-test-photos";
+
+                                                                request = BuildA.Request()
+                                .WithApplicationId("a5KqkemeX2RttyYdkOrdug")
+                                .WithSourceImageUri(new Uri("http://www.google.co.uk"))
+                                .SourceIsScreenshot()
+                                .WithCropFunction(f => f.WithDimensions(51, 126, 457 - 126, 382 - 51)
+                                                        .SaveAs(s => s.WithImageIdentifier("image_identifier")
+                                                                      .WithS3Destination(s3 => s3
+                                                                                  .WithBucketName(bucketName)
+                                                                                  .WithKey("screenshot.png")
+                                                                                  .Build())
+                                                                       .Build())
+                                                        .Build())
+                                .Build();
+                                                            });
+
+            "When I process the request".Do(() => response = request.Send());
+
+            "Then the s3 url should not be empty".Observation(() => Assert.NotEmpty(response.Results.Images.First().S3Url));
+        } 
+    }
+}
